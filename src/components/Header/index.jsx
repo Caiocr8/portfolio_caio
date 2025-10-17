dc22c2f06c2230165c7948dbcd54d9762be4211c
Caiocr8 <ccaiocr@gmail>
2025-10-17 19:29:57 -0300

    fix: remove bg-header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,7 @@
 
 const Header = () => {
   return (
-    <header className=" bg-cover flex justify-center p-5 bg-center  bg-header">
+    <header className=" bg-cover flex justify-center p-5 bg-center bg-[#2B2B2B]">
         <div className="mx-auto">
             <h1 className="text-white text-3xl font-bold tracking-widest">Caio Andrade</h1>
             <h2 className="text-white text-sm font-bold opacity-75">Full Stack + Heavy Frontend </h2>
